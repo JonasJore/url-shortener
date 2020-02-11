072bbebb3dd23ebf12a00c193d4f5be9b329c638
JonasJore <jonas.jore@gmail.com>
2020-02-11 06:24:48 +0100

    #21 - generate random hashed string for unique id everytime

---
+++ src/main/kotlin/util/AlphanumericHashGenerator.kt
@@ -0,0 +1,15 @@
+package util
+
+import kotlin.random.Random
+
+const val STRING_LENGTH: Int = 10
+
+class AlphanumericHashGenerator {
+  private val charPool: List<Char> = ('a'..'z') + ('A'..'Z') + ('0'..'9')
+
+  fun generateHash(): String = (1..STRING_LENGTH)
+      .map { Random.nextInt(0, charPool.size) }
+      .map { index: Int -> charPool[index] }
+      .joinToString("")
+
+}
