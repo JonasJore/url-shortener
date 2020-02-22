375036e609238ad5fe368538763d0d5d725de36a
JonasJore <jonas.jore@gmail.com>
2020-02-22 17:06:20 +0100

    redirection funker endelig :party: #37

--- src/main/kotlin/endpoints/RedirectResource.kt
+++ src/main/kotlin/endpoints/RedirectResource.kt
@@ -1,11 +1,7 @@
 package endpoints
 
-import org.apache.http.client.methods.RequestBuilder
-import org.apache.http.impl.client.DefaultRedirectStrategy
-import org.apache.http.impl.client.HttpClientBuilder
 import org.apache.log4j.Logger
 import service.RedirectionService
-import java.net.http.HttpResponse
 import javax.ws.rs.GET
 import javax.ws.rs.Path
 import javax.ws.rs.PathParam
@@ -21,11 +17,7 @@ class RedirectResource {
   @Path("/{uniqueurl}")
   @GET
   fun gotoUrl(@PathParam("uniqueurl") uniqueUrl: String): Response {
-    redirectionService.redirectToUrl(uniqueUrl)
-
-    return Response.status(302).build()
+    return redirectionService.redirectToUrl(uniqueUrl)
   }
-//  } catch (ex: Exception) {
-//    logger.error("something wrong while redirecting", ex)
-//  }
+
 }
--- src/main/kotlin/service/RedirectionService.kt
+++ src/main/kotlin/service/RedirectionService.kt
@@ -1,49 +1,31 @@
 package service
 
-import org.apache.http.client.methods.RequestBuilder
-import org.apache.http.impl.client.DefaultRedirectStrategy
-import org.apache.http.impl.client.HttpClientBuilder
-import org.apache.http.util.EntityUtils
 import org.apache.log4j.Logger
-import java.net.URI
 import java.net.URL
-
+import RedirectionException
+import javax.ws.rs.core.Response
 
 private val logger: Logger = Logger.getLogger(RedirectionService::class.java)
 
 class RedirectionService {
   private val urlShortenerService = UrlShortenerService()
-  private fun redirectHtml(uri: URI): String = "<meta http-equiv=\"refresh\" content=\"0; URL='${uri}'\" />"
 
-  private fun buildUrl(identifier: String) {
-    val redirectUri = URL(identifier).toURI()
+  // TODO: rydd opp senere
+  private fun buildUrl(identifier: String): Response {
+    val redirectUri = URL("https://$identifier").toURI()
+
     logger.info("redirecting to $redirectUri")
 
-    val client = HttpClientBuilder
-        .create()
-        .setRedirectStrategy(DefaultRedirectStrategy())
-        .build()
-
-    val request = RequestBuilder.get()
-        .setUri(redirectUri)
-        .setHeader("Location", identifier)
-        .setHeader("Redirect", identifier)
-        .setHeader("Referer", "https://www.google.com")
-        .build()
-
-    logger.info("================")
-    logger.info(request.allHeaders)
-
-    client.execute(request)
-
-//    return EntityUtils.toString(client.execute(request).entity)
-//    return redirectHtml(URL(identifier).toURI())
-
+    return try {
+      Response.status(
+          Response.Status.TEMPORARY_REDIRECT
+      ).location(redirectUri).build()
+    } catch (ex: RedirectionException) {
+      logger.info(ex)
+      Response.status(Response.Status.INTERNAL_SERVER_ERROR).build()
+    }
   }
 
   fun redirectToUrl(identifier: String) =
-//      buildUrl(urlShortenerService.getByIdOrShortened(identifier).url)
-      buildUrl("https://www.finn.no")
-
-
+      buildUrl(urlShortenerService.getByIdOrShortened(identifier).url)
 }
