5185fe0b05d737439c725c2de840b86ee52f884b
JonasJore <jonas.jore@gmail.com>
2020-02-25 07:07:34 +0100

    if url is not set, the service will appen https:// to the beginning of the urlString #40

--- src/main/kotlin/service/RedirectionService.kt
+++ src/main/kotlin/service/RedirectionService.kt
@@ -6,33 +6,33 @@ import javax.ws.rs.core.Response
 
 private val logger: Logger = Logger.getLogger(RedirectionService::class.java)
 
-const val MATCH_ON_PROTOCOL = "^(?:(http)(s?)://)"
+private const val MATCH_ON_PROTOCOL = "^(?:(http)(s?)://)"
 
 class RedirectionService {
   private val urlShortenerService = UrlShortenerService()
 
-  private fun hasProtocol(url: String): Boolean {
+  private fun hasProtocol(url: String): Boolean =
+      MATCH_ON_PROTOCOL.toRegex()
+          .let { it containsMatchIn url }
 
-    return Regex(MATCH_ON_PROTOCOL)
-        .let { it containsMatchIn  url }
+  private fun buildUrl(redirectUrlString: String): Response =
+      URL(redirectUrlString)
+          .toURI()
+          .let {
+            Response.status(
+                Response.Status.MOVED_PERMANENTLY
+            ).location(it).build()
+          }
+
+
+  fun redirectToUrl(identifier: String): Response {
+    val url = urlShortenerService.getByIdOrShortened(identifier).url
+    logger.info("redirecting to $url")
+    return when {
+      !hasProtocol(url) -> buildUrl("https://$url")
+      else -> buildUrl(url)
+    }
   }
-
-  // TODO: legg try/catch i endepunktet istedenfor her
-  private fun buildUrl(identifier: String): Response {
-    val redirectUri = URL(identifier).toURI()
-    logger.info("redirecting to $redirectUri")
-
-    logger.info(hasProtocol("================"))
-    logger.info(hasProtocol(identifier))
-
-    return Response.status(
-        Response.Status.MOVED_PERMANENTLY
-    ).location(redirectUri).build()
-  }
-
-  fun redirectToUrl(identifier: String) =
-      buildUrl(urlShortenerService.getByIdOrShortened(identifier).url)
-
 }
 
 infix fun Regex.containsMatchIn(s: String): Boolean =
