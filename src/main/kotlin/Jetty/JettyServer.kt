7932641ecea50f24a7fda7160aaa4725ee1fdeff
JonasJore <jonas.jore@gmail.com>
2020-02-28 07:33:05 +0100

    Codesplitting for jettyserver
    * gotten rid of unneccessary codelines
    * removed logging warnings
    * splitt providerclass into its own kotlin class
    #51

---
+++ src/main/kotlin/Jetty/JettyServer.kt
@@ -0,0 +1,30 @@
+package Jetty
+
+import org.apache.log4j.Logger
+import org.eclipse.jetty.server.Server
+import org.glassfish.jersey.jetty.JettyHttpContainerFactory
+import org.glassfish.jersey.server.ResourceConfig
+import javax.ws.rs.core.UriBuilder
+
+class JettyServer {
+  private val logger: Logger = Logger.getLogger(JettyServer::class.java)
+
+  fun startServer() {
+    logger.info("Firing up the server")
+    JettyHttpContainerFactory.createServer(
+        UriBuilder
+            .fromUri("http://localhost/")
+            .port(8080)
+            .build(),
+        ResourceConfig()
+            .register(ObjectMapperProvider::class.java)
+            .packages("endpoints")
+    ).use { server: Server -> server.join() }
+  }
+}
+
+inline fun Server.use(block: (Server) -> Unit) = try {
+  block(this)
+} finally {
+  this.destroy()
+}
