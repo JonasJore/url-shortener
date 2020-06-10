1ac37bb82b728ba7d598631c454e5bf5dd086ab2
JonasJore <jonas.jore@gmail.com>
2020-06-10 15:40:39 +0200

    merge from remote

---
+++ src/main/kotlin/jetty/ObjectMapperProvider.kt
@@ -0,0 +1,14 @@
+package jetty
+
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.databind.SerializationFeature
+import javax.ws.rs.ext.ContextResolver
+import javax.ws.rs.ext.Provider
+
+@Provider
+class ObjectMapperProvider : ContextResolver<ObjectMapper> {
+  override fun getContext(p0: Class<*>?): ObjectMapper? =
+      ObjectMapper()
+          .enable(SerializationFeature.INDENT_OUTPUT)
+          .disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS)
+}
