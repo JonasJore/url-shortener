496666819ac769c4f10029caf16cfc2a14aca781
JonasJore <jonas.jore@gmail.com>
2021-04-04 14:49:23 +0200

    update editorconfig and optimized imports

--- src/main/kotlin/endpoints/ShortenedUrlResource.kt
+++ src/main/kotlin/endpoints/ShortenedUrlResource.kt
@@ -1,22 +1,13 @@
 package endpoints
 
 import com.fasterxml.jackson.databind.ObjectMapper
-import com.fasterxml.jackson.databind.SerializationFeature
-import domain.ShortenedUrlDTO
 import domain.ShortenedUrl
-import domain.ShortenedUrls
+import domain.ShortenedUrlDTO
 import domain.UnshortenedUrlResponse
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 import service.UrlShortenerService
-import javax.ws.rs.Consumes
-import javax.ws.rs.DELETE
-import javax.ws.rs.GET
-import javax.ws.rs.POST
-import javax.ws.rs.PUT
-import javax.ws.rs.Path
-import javax.ws.rs.PathParam
-import javax.ws.rs.Produces
+import javax.ws.rs.*
 import javax.ws.rs.core.MediaType
 import javax.ws.rs.core.Response
 
--- src/main/kotlin/service/UrlShortenerService.kt
+++ src/main/kotlin/service/UrlShortenerService.kt
@@ -39,4 +39,3 @@ class UrlShortenerService {
     urlShortenerRepository.updateById(id, shortenedUrlDTO)
   }
 }
-
